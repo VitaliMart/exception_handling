1ae73352e1f4478053a18454ae7acb16cc3d76c6
Vitali Martsinchyk <vitali.martinchik@gmail.com>
2023-07-03 15:21:40 +0300

    feat:add decide HW

---
+++ exceptionPracticalTask.js
@@ -0,0 +1,17 @@
+
+
+
+try {
+    console.log(a);
+    let a = 3;
+} catch (err) {
+    console.log('let must be declare');
+};
+
+
+try {
+    1 / 0
+    if (1 / 0 === indefinat) throw Error()
+} catch (error) {
+    console.log('cannot be divided by zero');
+};
